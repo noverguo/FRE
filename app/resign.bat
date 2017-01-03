1f6d23620d00c64e90a8b90e032b00b7d36bb19b
noverguo <noverguo@gmail.com>
2017-01-03 16:02:49 +0800

    Delete resign.bat

--- app/resign.bat
+++
@@ -1,3 +0,0 @@
-jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore noverguo.jks -storepass noverguo2015.com -signedjar fre_1.apk fre_e_f_r.apk nv
-zipalign.exe -v 4 fre_1.apk fre_1.2.5.apk
-del fre_1.apk
